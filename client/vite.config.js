506870de9bc53542a3645f4f2cac0fb6d9f70167
RobelMessi <robelyoseph1247@gmail.com>
2026-05-27 19:50:53 -0400

    Reset frontend to barebones skeleton

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // Add this import
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Add this here
-  ],
+  plugins: [react()],
 });
